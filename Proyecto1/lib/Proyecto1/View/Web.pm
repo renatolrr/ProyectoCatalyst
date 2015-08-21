b022a6ee2036b426895fef5d8428dca12ebb3fcd
renatolrr <renatolrr@h1950820.stratoserver.net>
2015-08-21 19:08:50 +0200

    Creando Template

---
+++ Proyecto1/lib/Proyecto1/View/Web.pm
@@ -0,0 +1,35 @@
+package Proyecto1::View::Web;
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+);
+
+=head1 NAME
+
+Proyecto1::View::Web - TT View for Proyecto1
+
+=head1 DESCRIPTION
+
+TT View for Proyecto1.
+
+=head1 SEE ALSO
+
+L<Proyecto1>
+
+=head1 AUTHOR
+
+,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ Proyecto1/t/view_Web.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'Proyecto1::View::Web' }
+
+done_testing();
